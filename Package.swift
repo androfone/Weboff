5f75d45ebe735231d275f815e3b8c30954c09765
LULUKit <123129846+SAMISsw@users.noreply.github.com>
2024-10-29 18:12:25 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "Weboff",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: "Weboff",
+            targets: ["Weboff"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "MyLibrary",
+            name: "Weboff",
             dependencies: []),
         .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+            name: "Weboff",
+            dependencies: ["Weboff"]),
     ]
 )
